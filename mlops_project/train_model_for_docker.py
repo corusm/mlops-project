0e92e28ec5638ba04765635cfed6756d0d589811
corusm <dev@corusm.de>
2024-01-18 00:50:11 +0100

    update train_model_for_docker

--- mlops_project/train_model_for_docker.py
+++ mlops_project/train_model_for_docker.py
@@ -1,21 +1,36 @@
-# Use this script inside docker to run sequentially:
-# - process data
-# - train
-# - visualize
-
 import subprocess
+import sys
+import os
 
-
-def run_script(script_name):
+def run_script(script_path):
     try:
-        # Run the script
-        subprocess.run(["python", script_name], check=True)
-        print(f"Successfully executed {script_name}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while executing {script_name}: {e}")
+        # Print current working directory and PYTHONPATH
+        print(f"Current working directory: {os.getcwd()}")
+        python_path = os.environ.get('PYTHONPATH', '')
+        print(f"PYTHONPATH: {python_path}")
 
+        # Full path to script
+        full_script_path = os.path.join(os.getcwd(), script_path)
+        print(f"Full script path: {full_script_path}")
+
+        # Check if the script file exists at the expected path
+        if not os.path.isfile(full_script_path):
+            raise FileNotFoundError(f"The script {full_script_path} does not exist.")
+
+        # Run the script
+        subprocess.run(["python3", full_script_path], check=True)
+        print(f"Successfully executed {script_path}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while executing {script_path}: {e}")
+        print(f"Return code: {e.returncode}")
+        if e.output:
+            print(f"Output: {e.output.decode('utf-8')}")
+        if e.stderr:
+            print(f"Error: {e.stderr.decode('utf-8')}")
+    except FileNotFoundError as e:
+        print(e)
 
 if __name__ == "__main__":
     run_script("mlops_project/data/make_dataset.py")
-    run_script("mlops_project/train_model.py")
+    run_script("mlops_project/models/train_model.py")
     run_script("mlops_project/visualizations/visualize.py")
