38a28d43a589dc6c5034db01ee94c2fb0eb80f3b
Niklas Leinz <dev@corusm.de>
2024-01-19 00:44:02 +0100

    Revert "Implements Haydra and sweeps (sweeps not yet in Docker)"
    This reverts commit 19f30886d7950213d99a08f0ed55b3e430892ab8.

--- mlops_project/models/model.py
+++ mlops_project/models/model.py
@@ -1,5 +1,3 @@
-import hydra
-import omegaconf
 from tsai.basics import TSForecaster, load_object, mse, mae, np, os
 
 import wandb
@@ -46,54 +44,69 @@ class Forecaster:
 
         return X, y, preproc_pipe, exp_pipe, splits
 
-
-    @hydra.main(config_path="configs/", config_name="config")
-    def train_model(self) -> Any:
+    def train_model(self):
         PATH_PROCESSED = "data/processed"
 
         X, y, preproc_pipe, exp_pipe, splits = self.get_data(PATH_PROCESSED)
 
-        wandb.config = omegaconf.OmegaConf.to_container(
-            cfg, resolve=True, throw_on_missing=True
-        )
+        sweep_config = {
+            'method':'bayes',
+            'parameters': {
+                'n_layers': {
+                    'values':[1, 2, 3, 4, 5]
+                    },
+                'd_model': {
+                    'values': [8, 16, 32, 48, 64]
+                    },
+                'd_ff':{
+                    'values': [128, 192, 256, 320, 384]
+                    },
+                'dropout': { 
+                    'values': [0, 0.3, 0.6]
+                    }
+            },
+            'metric': {
+                'name': 'valid_loss',
+                'goal': 'minimize'
+                }
+        }
+        sweep_id = wandb.sweep(sweep_config, project="62-train")
 
-        with wandb.init(project="dtu-mlops-model-reg-62") as run:
+        def train_conf():
+            with wandb.init():
+                arch_config = dict(
+                    n_layers=wandb.config.n_layers,  # number of encoder layers
+                    n_heads=4,  # number of heads
+                    d_model=wandb.config.d_model,  # dimension of model
+                    d_ff=wandb.config.d_ff,  # dimension of fully connected network
+                    attn_dropout=0.0,  # dropout applied to the attention weights
+                    dropout=wandb.config.dropout,  # dropout applied to all linear layers in the encoder except q,k&v projections
+                    patch_len=24,  # length of the patch applied to the time series to create patches
+                    stride=2,  # stride used when creating patches
+                    padding_patch=True,  # padding_patch
+                )
 
-            arch_config = dict(
-                n_layers=wandb.config.n_layers,  # number of encoder layers
-                n_heads=4,  # number of heads
-                d_model=wandb.config.d_model,  # dimension of model
-                d_ff=wandb.config.d_ff,  # dimension of fully connected network
-                attn_dropout=0.0,  # dropout applied to the attention weights
-                dropout=wandb.config.dropout,  # dropout applied to all linear layers in the encoder except q,k&v projections
-                patch_len=24,  # length of the patch applied to the time series to create patches
-                stride=2,  # stride used when creating patches
-                padding_patch=True,  # padding_patch
-            )
+                learn = TSForecaster(
+                    X,
+                    y,
+                    splits=splits,
+                    batch_size=16,
+                    path="models",
+                    pipelines=[preproc_pipe, exp_pipe],
+                    arch="PatchTST",
+                    arch_config=arch_config,
+                    metrics=[mse, mae],
+                    cbs=WandbCallback()
+                )
 
-            learn = TSForecaster(
-                X,
-                y,
-                splits=splits,
-                batch_size=16,
-                path="models",
-                pipelines=[preproc_pipe, exp_pipe],
-                arch="PatchTST",
-                arch_config=arch_config,
-                metrics=[mse, mae],
-                cbs=WandbCallback()
-            )
+                n_epochs = 20
+                lr_max = 0.0025
 
-            n_epochs = 20
-            lr_max = 0.0025
-
-            learn.fit_one_cycle(n_epochs, lr_max=lr_max)
-            
-            # save model
-            learn.export("patchTST.pt")
-
-            model_artifact = wandb.Artifact(name="model_62", type="model")
-            model_artifact.add_file("patchTST.pt")
-            run.log_artifact(model_artifact)
+                learn.fit_one_cycle(n_epochs, lr_max=lr_max)
+                
+                # save model
+                learn.export("patchTST.pt")
+        
+        wandb.agent(sweep_id, train_conf, count=5)
 
         # TODO make a plot
