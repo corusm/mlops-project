600a77fa9f3b325ed28a8ff217474f9d0cf8d7a2
Niklas Leinz <dev@corusm.de>
2024-01-19 03:45:13 +0100

    Fix path issues for continious deployment (#21)

--- mlops_project/models/model.py
+++ mlops_project/models/model.py
@@ -70,27 +70,27 @@ def train_model(cfg) -> None:
             cbs=WandbCallback(),
         )
 
-        n_epochs = 20
+        n_epochs = 5
         lr_max = 0.0025
 
         learn.fit_one_cycle(n_epochs, lr_max=lr_max)
 
         # Save model
-        learn.export("models/patchTST.pt")
+        learn.export("/__w/mlops-project/mlops-project/patchTST.pt")
         model_artifact = wandb.Artifact(name="model_62", type="model")
-        model_artifact.add_file("models/patchTST.pt")
-        model_artifact.add_file("data/processed/preproc_pipe.pkl")
-        model_artifact.add_file("data/processed/exp_pipe.pkl")
-        model_artifact.add_file("data/processed/splits.pkl")
+        model_artifact.add_file("/__w/mlops-project/mlops-project/patchTST.pt")
+        model_artifact.add_file("/__w/mlops-project/mlops-project/data/processed/preproc_pipe.pkl")
+        model_artifact.add_file("/__w/mlops-project/mlops-project/data/processed/exp_pipe.pkl")
+        model_artifact.add_file("/__w/mlops-project/mlops-project/data/processed/splits.pkl")
         run.log_artifact(model_artifact)
         run.link_artifact(model_artifact, "model-registry/registered_model")
 
         # Save plots
         plot_splits(splits)
         plt.tight_layout()  # fix label cutoff issue
-        plt.savefig("models/splits.png")
+        plt.savefig("splits.png")
         plot_artifact = wandb.Artifact(name="plots_62", type="plot")
-        plot_artifact.add_file("models/splits.png")
+        plot_artifact.add_file("splits.png")
         run.log_artifact(plot_artifact)
 
         run.finish()
