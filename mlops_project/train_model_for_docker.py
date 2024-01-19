62944b0f9ffd1bd6b00f2311d17b003a60a6e16b
corusm <dev@corusm.de>
2024-01-19 06:13:47 +0100

    update

--- mlops_project/train_model_for_docker.py
+++ mlops_project/train_model_for_docker.py
@@ -8,4 +8,4 @@ import subprocess
 if __name__ == "__main__":
     subprocess.run(["python", "mlops_project/data/make_dataset.py"], check=True)
     subprocess.run(["python", "mlops_project/train_model.py"], check=True)
-    subprocess.run(["python", "mlops_project/visualizations/visualize.py"], check=True)
+#    subprocess.run(["python", "mlops_project/visualizations/visualize.py"], check=True)
